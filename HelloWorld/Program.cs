f2dbbc8b77744fc8f4dd965ab9911ce9d1be4ac1
Harshvardhan <harshvardhanvipat@gmail.com>
2020-11-19 08:56:48 +1100

    Created a guessing game using C# basics loops and conditional statements

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,39 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("This is the test project ");
+            Random r = new Random();
+            int winNum = r.Next(0, 10);
+            bool win = false;
+            do
+            {
+                Console.Write("Guess a number between 0 and 100");
+                string s = Console.ReadLine();
+                // this line will error if user inputs something else then a number
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too high, guess lower !!!");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Too low, guess higher !!!");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("You Win");
+                    win = true;
+                }
+                Console.WriteLine();
+            }
+            while (win == false);
+            Console.WriteLine("Thank you playing the game !!");
+            Console.WriteLine("Press any key to finish!!");
+            Console.ReadKey(true);
+
+
+
+
         }
     }
 }
